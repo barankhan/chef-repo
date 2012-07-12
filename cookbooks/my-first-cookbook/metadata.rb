7d281aa7104bedc2babd59a891e32780e4e51252
BaranKhan <barankhan@barankhan-VirtualBox.(none)>
2012-07-12 21:42:19 +0500

    Added my-first-cookbook and helloworld recipe

---
+++ cookbooks/my-first-cookbook/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Insitve"
+maintainer_email "selfcoordinator@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures my-first-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
